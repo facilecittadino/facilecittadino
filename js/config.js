23b5654e2b63cc860e1ded7cbeedc2b4a886e693
facilecittadino <facilecittadino@gmail.com>
2025-11-01 14:39:53 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 window.APP_CONFIG = {
   LARGE_BP: 992,                 // desktop breakpoint
-  EXPIRY_MS: 60 * 60 * 1000,
+  EXPIRY_MS: 10 * 1000,
   STORAGE_KEYS: {
     LANG: "apll.lang"
   }
